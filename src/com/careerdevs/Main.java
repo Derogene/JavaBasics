8853c865b3cbcb91589ae919403da81fdb6bf045
Derogene <96089543+Derogene@users.noreply.github.com>
2022-01-06 17:16:46 -0500

    Done 1/3

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -3,6 +3,63 @@ package com.careerdevs;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String firstName = "Rodney";
+        String lastName = "Derogene";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
     }
+
 }
+
+//        String createdName = createFullName(firstName, lastName);
+//        System.out.println(createdName);
+//
+//         Person myPerson = new  Person(firstName, lastName);
+//         System.out.println("Firstname: " + myPerson.firstName);
+//         System.out.println("Lastname: " + myPerson.lastName);
+//         System.out.println(myPerson.getFullName());
+//
+//            Person[] allPeople = new Person[People.firstNames.length];
+//
+//            for (int i = 0; i < People.firstNames.length ; i++) {
+//
+////                System.out.println(People.lastNames[i]);
+//                String fName = People.firstNames[i];
+//                String lName = People.lastNames[i];
+//
+//                Person tempPerson = new Person(fName, lName);
+//                //add the new Person to the array of Persons
+//                allPeople[i] = tempPerson;
+//
+//            }
+//
+//            System.out.println(allPeople);
+//
+//
+//
+//    }
+//
+//    public static String createFullName (String fName, String lName) {
+//        return fName + " " + lName;
+//    }
+//}
+//
+//// line 12 - 13 has same output then 9-10
+//
+//    //traditional arrays
+//    Person[] allPeople = new Person[People.firstNames.length];
+//
+//    //array list
+//    ArrayList <Person> listofPeople = new ArrayList<>();
+//
+////add the new Person to the array of Persons
+//allPeople[i] = tempPerson;
+//
+////add to a list
+//        listOfPeople.add(tempPerson);
+//
+////correspondence
+//// this.firstNme ~ field
+//// fieldName ~ parameter
+
